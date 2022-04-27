22855ed6dc7e4886ac9bb326dc48ca9e02f72e2f
xdaiyan <daiyanch22@gmail.com>
2022-04-27 19:43:32 -0400

    UndergroundSystem Completion

--- Design_Underground_System/UnderGroundSystem_python_solver.py
+++ Design_Underground_System/UnderGroundSystem_python_solver.py
@@ -11,18 +11,21 @@ class underground_system(object):
 
     def check_in(self, id, station_name, time) -> None:
         """
+        A customer with a card ID equal to id, checks in at the station stationName at time t.
+        A customer can only be checked into one place at a time.
         :type id: int
         :type stationName: str
-        :type t: int
+        :type time: int
         :rtype: None
         """
         self.check_in[id] = (station_name, time)
 
     def check_out(self, id, station_name, time) -> None:
         """
+        A customer with a card ID equal to id, checks out from the station stationName at time t.
         :type id: int
         :type stationName: str
-        :type t: int
+        :type time: int
         :rtype: None
         """
 
@@ -37,6 +40,13 @@ class underground_system(object):
 
     def get_average_time(self, start_station, end_station) -> float:
         """
+        Returns the average time it takes to travel from startStation to endStation.
+        The average time is computed from all the previous traveling times from startStation to endStation that happened directly, 
+        meaning a check in at startStation followed by a check out from endStation.
+        The time it takes to travel from startStation to endStation may be different from the time it takes to travel from endStation to startStation.
+        There will be at least one customer that has traveled from startStation to endStation before getAverageTime is called.
+        You may assume all calls to the checkIn and checkOut methods are consistent. If a customer checks in at time t1 then checks out at time t2, then t1 < t2. 
+        All events happen in chronological order.
         :type startStation: str
         :type endStation: str
         :rtype: float
